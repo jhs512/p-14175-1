98c39624060a7234283f6824c625702e69ec632a
jhs512 <jangka512@gmail.com>
2026-05-28 12:34:52 +0900

    006

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,22 @@ export default function RootLayout({
   return (
     <html
       lang="ko"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="flex flex-col">
+        <header>
+          <nav className="flex">
+            <a href="/" className="p-2 rounded hover:bg-gray-100">
+              메인
+            </a>
+            <a href="/posts" className="p-2 rounded hover:bg-gray-100">
+              글 목록
+            </a>
+          </nav>
+        </header>
+        <main>{children}</main>
+        <footer>푸터</footer>
+      </body>
     </html>
   );
 }
