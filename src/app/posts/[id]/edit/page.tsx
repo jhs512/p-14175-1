b14b731c568c7844b51a737c8d08e5b35afbaacd
jhs512 <jangka512@gmail.com>
2026-07-09 10:52:18 +0900

    029

--- src/app/posts/[id]/edit/page.tsx
+++ src/app/posts/[id]/edit/page.tsx
@@ -2,10 +2,12 @@
 
 import { apiFetch } from "@/lib/backend/client";
 import type { PostWithContentDto } from "@/type/post";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Page() {
+  const router = useRouter();
+
   const { id: idStr } = useParams<{ id: string }>();
   const id = Number(idStr);
 
@@ -17,11 +19,50 @@ export default function Page() {
 
   if (post == null) return <div>로딩중...</div>;
 
+  const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+
+    const titleInput = form.elements.namedItem("title") as HTMLInputElement;
+    const contentTextarea = form.elements.namedItem(
+      "content",
+    ) as HTMLTextAreaElement;
+
+    titleInput.value = titleInput.value.trim();
+
+    if (titleInput.value.length === 0) {
+      alert("제목을 입력해주세요.");
+      titleInput.focus();
+      return;
+    }
+
+    contentTextarea.value = contentTextarea.value.trim();
+
+    if (contentTextarea.value.length === 0) {
+      alert("내용을 입력해주세요.");
+      contentTextarea.focus();
+      return;
+    }
+
+    apiFetch(`/api/v1/posts/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        title: titleInput.value,
+        content: contentTextarea.value,
+      }),
+    }).then((data) => {
+      alert(data.msg);
+
+      router.replace(`/posts/${id}`);
+    });
+  };
+
   return (
     <>
       <h1>{id}번 글 수정</h1>
 
-      <form className="flex flex-col gap-2 p-2">
+      <form className="flex flex-col gap-2 p-2" onSubmit={handleSubmit}>
         <input
           className="border p-2 rounded"
           type="text"
